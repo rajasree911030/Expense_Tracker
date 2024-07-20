c5401b867e369f0ac2ff0f14f521dd0441f3e843
rajasree911030 <sheshagonirajasri@gmail.com>
2024-07-20 20:12:31 +0530

    Add files via upload

---
+++ register.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import axios from '../axios';
+
+function Register() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/users/register', { username, email, password });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleRegister}>
+      <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Register</button>
+    </form>
+  );
+}
+
+export default Register;
